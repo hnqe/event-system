cb6d4094099d7f3404bd90cea76f9d5d3227009a
hnrq <hnrqog@gmail.com>
2025-03-18 05:53:13 -0300

    fix issues: some adjusts

--- event-system/src/main/java/br/edu/ifg/event_system/config/SecurityConfig.java
+++ event-system/src/main/java/br/edu/ifg/event_system/config/SecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
